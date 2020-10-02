7523b3a3c216a9a3c90c1f964452b2e4c9a7bdda
Stephen Odumirin <Stephenodumirin@gmail.com>
2020-10-02 11:11:10 +0100

    Created a new math riddle (#4)

---
+++ lib/categories/Math/math2@Stephen-Odumirin/math2HomePage.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../../../universal/result.dart';
+import '../../../universal/riddleAppbar.dart';
+
+class Math2HomeScreen extends StatefulWidget {
+  @override
+  _Math2HomeScreenState createState() => _Math2HomeScreenState();
+}
+
+class _Math2HomeScreenState extends State<Math2HomeScreen> {
+  int number;
+  bool _isScore = false;
+  int totalScore = 0;
+
+  void resetHanlder() {
+    setState(() {
+      totalScore = 0;
+      _isScore = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RiddleBar(
+      title: 'FUNNY DIVISION',
+      body: _isScore
+          ? Result(totalScore, 1, resetHanlder, '/mathPage')
+          : Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1.6),
+                            color: Colors.transparent),
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'HOW MANY TIMES CAN YOU DIVIDE THE NUMBER 100 BY 10 ?',
+                              style: TextStyle(fontSize: 15),
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1.6),
+                          color: Colors.transparent),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Answer',
+                                hintText: 'Enter your answer in numbers !'),
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) => number = int.parse(value),
+                          ),
+                          SizedBox(
+                            height: 60,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, right: 10, left: 10),
+                            child: InkWell(
+                              onTap: () {
+                                if (number == 1) {
+                                  setState(() {
+                                    totalScore = 1;
+                                    _isScore = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    totalScore = 0;
+                                    _isScore = true;
+                                  });
+                                }
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 1.6),
+                                    color: Colors.transparent),
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Text(
+                                  'SUBMIT',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
