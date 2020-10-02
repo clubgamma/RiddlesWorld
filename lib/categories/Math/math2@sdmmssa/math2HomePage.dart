54be8f6658a5db09b244642fe04666d3790d7494
sdmmssa <33046648+sdmmssa@users.noreply.github.com>
2020-10-01 20:47:34 -0500

    Create math2HomePage.dart

---
+++ lib/categories/Math/math2@sdmmssa/math2HomePage.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:riddleworld/universal/result.dart';
+import 'package:riddleworld/universal/riddleAppbar.dart';
+
+class Math1HomeScreen extends StatefulWidget {
+  @override
+  _Math1HomeScreenState createState() => _Math1HomeScreenState();
+}
+
+class _Math1HomeScreenState extends State<Math1HomeScreen> {
+  int number;
+  bool _isScore = false;
+  int totalScore = 0;
+
+  void resetHanlder() {
+    setState(() {
+      totalScore = 0;
+      _isScore = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RiddleBar(
+      title: 'FUNNY SUBTRACTION',
+      body: _isScore
+          ? Result(totalScore, 1, resetHanlder, '/mathPage')
+          : Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1.6),
+                            color: Colors.transparent),
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'HOW MANY TIMES CAN YOU SUBTRACT THE NUMBER 5 FROM 25?',
+                              style: TextStyle(fontSize: 15),
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1.6),
+                          color: Colors.transparent),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Answer',
+                                hintText: 'Enter answer in numbers!!'),
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) => number = int.parse(value),
+                          ),
+                          SizedBox(
+                            height: 60,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, right: 10, left: 10),
+                            child: InkWell(
+                              onTap: () {
+                                if (number == 1) {
+                                  setState(() {
+                                    totalScore = 1;
+                                    _isScore = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    totalScore = 0;
+                                    _isScore = true;
+                                  });
+                                }
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 1.6),
+                                    color: Colors.transparent),
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Text(
+                                  'SUBMIT',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
