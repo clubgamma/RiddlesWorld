e0900e23eb014a8b48098cf1fadfaf7d5fa4bc9a
sdmmssa <33046648+sdmmssa@users.noreply.github.com>
2020-10-03 07:39:06 -0500

    Updated pull request (#15)
    * Create math4HomePage.dart
    
    * Create math4@sdmmssa
    
    * Delete math4@sdmmssa
    
    * Rename lib/categories/Math/Math4@sdmmssa/math4HomePage.dart to lib/categories/Math/math4@sdmmssa/math4HomePage.dart
    
    * Update MathRiddlelLists.dart

---
+++ lib/categories/Math/math4@sdmmssa/math4HomePage.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:riddleworld/universal/result.dart';
+import 'package:riddleworld/universal/riddleAppbar.dart';
+
+class Math4HomeScreen extends StatefulWidget {
+  @override
+  _Math4HomeScreenState createState() => _Math4HomeScreenState();
+}
+
+class _Math4HomeScreenState extends State<Math4HomeScreen> {
+  int number;
+  bool _isScore = false;
+  int totalScore = 0;
+
+  void resetHandler() {
+    setState(() {
+      totalScore = 0;
+      _isScore = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RiddleBar(
+      title: 'AlgeNumbers',
+      body: _isScore
+          ? Result(totalScore, 1, resetHandler, '/mathPage')
+          : Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1.6),
+                            color: Colors.transparent),
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'X is an odd number. Take an alphabet away from X and it becomes even. Which is that number?',
+                              style: TextStyle(fontSize: 15),
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1.6),
+                          color: Colors.transparent),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Answer',
+                                hintText: 'Enter answer in numbers!!'),
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) => number = int.parse(value),
+                          ),
+                          SizedBox(
+                            height: 60,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, right: 10, left: 10),
+                            child: InkWell(
+                              onTap: () {
+                                if (number == 7) {
+                                  setState(() {
+                                    totalScore = 1;
+                                    _isScore = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    totalScore = 0;
+                                    _isScore = true;
+                                  });
+                                }
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 1.6),
+                                    color: Colors.transparent),
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Text(
+                                  'SUBMIT',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
