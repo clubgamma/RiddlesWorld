93502a0abbc5710225cf807aeee5bfa10e1e4097
c0decreat0r <68531496+c0decreat0r@users.noreply.github.com>
2020-10-07 15:19:05 +0530

    added new math puzzle (#27)
    Co-authored-by: Harsh Patel <harshptl714@gmail.com>

---
+++ lib/categories/Math/math8@c0decreat0r/math8HomePage.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:riddleworld/universal/result.dart';
+import 'package:riddleworld/universal/riddleAppbar.dart';
+
+class Math8HomeScreen extends StatefulWidget {
+  @override
+  _Math8HomeScreenState createState() => _Math8HomeScreenState();
+}
+
+class _Math8HomeScreenState extends State<Math8HomeScreen> {
+  String number;
+  bool _isScore = false;
+  int totalScore = 0;
+
+  void resetHandler() {
+    setState(() {
+      totalScore = 0;
+      _isScore = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RiddleBar(
+      title: 'Odd <-> Even',
+      body: _isScore
+          ? Result(totalScore, 1, resetHandler, '/mathPage')
+          : Center(
+              child: ListView(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1.6),
+                            color: Colors.transparent),
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Make SIX an even number...',
+                              style: TextStyle(fontSize: 15),
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1.6),
+                          color: Colors.transparent),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Answer',
+                                hintText: 'Enter answer in alphabets only!!'),
+                            keyboardType: TextInputType.name,
+                            onChanged: (value) => number = value.toUpperCase(),
+                          ),
+                          SizedBox(
+                            height: 60,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, right: 10, left: 10),
+                            child: InkWell(
+                              onTap: () {
+                                if (number == "IX") {
+                                  setState(() {
+                                    totalScore = 1;
+                                    _isScore = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    totalScore = 0;
+                                    _isScore = true;
+                                  });
+                                }
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 1.6),
+                                    color: Colors.transparent),
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Text(
+                                  'SUBMIT',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
